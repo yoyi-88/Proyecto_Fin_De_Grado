75acbfc60f283ad401e6a989ca1f83db7e022b19
yoyi-88 <ygomben2510@g.educaand.es>
2026-03-11 13:35:18 +0100

    Fix vistas User

--- views/user/show/index.php
+++ views/user/show/index.php
@@ -1,60 +1,44 @@
-<!doctype html>
-<html lang="es">
+<div class="container">
+    <br><br><br><br>
 
-<head>
-    <?php require_once 'template/layouts/head.layout.php'; ?>
-    <title><?= $this->title ?> </title>
-</head>
+    <?php require_once("template/partials/mensaje.partial.php") ?>
+    <?php require_once("template/partials/error.partial.php") ?>
 
-<body>
-    <?php require_once("template/partials/menu.partial.php") ?>
+    <main>
+        <legend><?= $this->title ?></legend>
+        <form>
 
-    <div class="container">
-        <br><br><br><br>
+            <div class="mb-3">
+                <label for="id" class="form-label">ID:</label>
+                <input type="text" class="form-control" value="<?= $this->user->id ?>" disabled>
+            </div>
 
-        <?php require_once("template/partials/mensaje.partial.php") ?>
-        <?php require_once("template/partials/error.partial.php") ?>
+            <div class="mb-3">
+                <label for="name" class="form-label">Nombre:</label>
+                <input type="text" class="form-control" value="<?= $this->user->name ?>" disabled>
+            </div>
 
-        <main>
-            <legend><?= $this->title ?></legend>
-            <form>
-                
-                <div class="mb-3">
-                    <label for="id" class="form-label">ID:</label>
-                    <input type="text" class="form-control" value="<?= $this->user->id ?>" disabled>
-                </div>
+            <div class="mb-3">
+                <label for="email" class="form-label">Email:</label>
+                <input type="text" class="form-control" value="<?= $this->user->email ?>" disabled>
+            </div>
 
-                <div class="mb-3">
-                    <label for="name" class="form-label">Nombre:</label>
-                    <input type="text" class="form-control" value="<?= $this->user->name ?>" disabled>
-                </div>
+            <div class="mb-3">
+                <label for="role" class="form-label">Rol Asignado:</label>
+                <input type="text" class="form-control" value="<?= $this->role_name ?>" disabled>
+            </div>
 
-                <div class="mb-3">
-                    <label for="email" class="form-label">Email:</label>
-                    <input type="text" class="form-control" value="<?= $this->user->email ?>" disabled>
-                </div>
-
-                <div class="mb-3">
-                    <label for="role" class="form-label">Rol Asignado:</label>
-                    <input type="text" class="form-control" value="<?= $this->role_name ?>" disabled>
-                </div>
-
-                <?php if(isset($this->user->created_at)): ?>
+            <?php if (isset($this->user->created_at)): ?>
                 <div class="mb-3">
                     <label class="form-label">Fecha de Creación:</label>
-                    <input type="text" class="form-control" value="<?= date('d/m/Y H:i:s', strtotime($this->user->created_at)) ?>" disabled>
+                    <input type="text" class="form-control"
+                        value="<?= date('d/m/Y H:i:s', strtotime($this->user->created_at)) ?>" disabled>
                 </div>
-                <?php endif; ?>
+            <?php endif; ?>
 
-                <a class="btn btn-secondary" href="<?= URL ?>user" role="button">Volver a la lista</a>
-                
-            </form>
-            <br><br><br>
-        </main>
-    </div>
+            <a class="btn btn-secondary" href="<?= URL ?>user" role="button">Volver a la lista</a>
 
-    <?php require_once("template/partials/footer.partial.php") ?>
-    <?php require_once("template/layouts/javascript.layout.php") ?>
-
-</body>
-</html>
+        </form>
+        <br><br><br>
+    </main>
+</div>
