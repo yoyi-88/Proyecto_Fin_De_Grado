7dbe8312b6822ffa5b66a87edcd21b30b839f293
yoyi-88 <ygomben2510@g.educaand.es>
2026-03-16 12:13:56 +0100

    Creación de variables y bootstrap

--- template/layouts/main.layout.php
+++ template/layouts/main.layout.php
@@ -5,25 +5,14 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?= $this->title ?? 'Chef Privado' ?></title>
     
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
+    <!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"> -->
+    <link rel="stylesheet" href="public/scss/main.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css">
     
     <link href="https://fonts.googleapis.com/css2?family=Playfair+Display:wght@400;700&family=Lato:wght@300;400;700&display=swap" rel="stylesheet">
 
     <link href="<?= URL ?>public/css/style.css" rel="stylesheet">
 
-    <style>
-        body {
-            font-family: 'Lato', sans-serif;
-            display: flex;
-            flex-direction: column;
-            min-height: 100vh;
-            padding-top: 56px; /* Ajuste fino para la navbar */
-            background-color: #f8f9fa; /* Fondo gris muy suave para toda la web */
-        }
-        h1, h2, h3, h4, .navbar-brand { font-family: 'Playfair Display', serif; }
-        main { flex: 1; }
-    </style>
 </head>
 <body>
 
