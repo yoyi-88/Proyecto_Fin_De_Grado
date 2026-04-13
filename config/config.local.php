e37ae913f78f7fc4c103e33ae16b8e398b07aaab
yoyi-88 <ygomben2510@g.educaand.es>
2026-04-13 11:16:31 +0200

    Cambios varios

---
+++ config/config.local.php
@@ -0,0 +1,28 @@
+<?php
+# Configuración básica aplicación MVC
+
+# Ruta absoluta del proyecto
+define('URL', 'https://localhost/Proyecto_Fin_De_Grado/');
+
+
+# Constantes de configuración de la aplicación
+define('APP_NAME', 'DeMiCasaALaTuya');
+define('APP_VERSION', '1.0.0');
+define('APP_AUTHOR', 'Yoël Gómez Benítez');
+
+# Rutas de los componentes MVC
+define('CONTROLLER_PATH', 'controllers/');
+define('MODEL_PATH', 'models/');
+define('VIEW_PATH', 'views/');
+define('DEFAULT_CONTROLLER', 'main');
+define('ERROR_CONTROLLER', 'error');
+
+# Constante de la Base de Datos
+define('HOST', 'localhost');
+define('DB', 'dmcalt');
+define('USER', 'root');
+define('PASSWORD', '');
+define('CHARSET', 'utf8mb4');
+
+
+?>
