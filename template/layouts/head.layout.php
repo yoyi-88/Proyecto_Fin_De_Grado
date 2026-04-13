b3de0ac4d921e308ba7f947de6551f6019a1b33b
yoyi-88 <ygomben2510@g.educaand.es>
2026-04-13 12:00:04 +0200

    Nuevas rutas css

--- template/layouts/head.layout.php
+++ template/layouts/head.layout.php
@@ -2,6 +2,6 @@
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
 <!-- css -->
-<link href="<?= URL ?>public/scss/main.css" rel="stylesheet">
+<link href="<?= URL ?>public/css/main.css" rel="stylesheet">
 <!-- bootstrap icons 1.13.1 -->
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.13.1/font/bootstrap-icons.min.css">
--- template/layouts/main.layout.php
+++ template/layouts/main.layout.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?= $this->title ?? 'Chef Privado' ?></title>
     
-    <link rel="stylesheet" href="<?= URL ?>public/scss/main.css">
+    <link rel="stylesheet" href="<?= URL ?>public/css/main.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css">
     
     <link href="https://fonts.googleapis.com/css2?family=Playfair+Display:wght@400;700&family=Lato:wght@300;400;700&display=swap" rel="stylesheet">
