a9f410e22856b6d563d8e2256da9ebb5f87b28ea
yoyi-88 <ygomben2510@g.educaand.es>
2026-04-13 11:18:42 +0200

    Cambio a local

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php  
-require_once 'config/config.php';
+require_once 'config/config.local.php';
 require_once 'libs/database.php';
 require_once 'libs/controller.php';
 require_once 'libs/model.php';
